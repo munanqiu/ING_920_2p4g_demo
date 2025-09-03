8f9fd366d239658b12181f0801e530c491f2c07b
Liulei <18511073226@163.com>
2025-09-03 17:12:58 +0800

    删掉了Event中断里的slave接收成功时的打印，否则无法测吞吐率

--- Prj_2p4g_with_lib/src/app_2p4g.c
+++ Prj_2p4g_with_lib/src/app_2p4g.c
@@ -6,7 +6,7 @@
 #include "app_2p4g.h"
 
 static uint8_t master_tx_len = 10;
-static uint8_t slave_tx_len = 15;
+static uint8_t slave_tx_len = 0;
 uint8_t tx_data[200]={0,5,4,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}; 
 static ING2P4G_RxPacket RxPkt111;
 static uint8_t continus_2g4 = 0;
@@ -128,17 +128,6 @@ ADDITIONAL_ATTRIBUTE static void EventIrqCallBack(void)
         }
         else
         {
-            if(status == ING2P4G_SUCCESS)
-            {
-                slave_tx_len = (slave_tx_len==15?255:15);
-                printf("Event cb Rx:%d, len:%d\n", status, RxPkt111.DataLen);
-                for(int i=0; i<RxPkt111.DataLen; i++)
-                    printf("%d ", RxPkt111.Data[i]);
-                printf("\n");
-            }
-            else{
-                printf("Event cb Rx:%d\n", status);
-            }
             ing2p4g_start_2p4g_rx(slave_tx_len, tx_data);
         }
     }
